b3225c4fed963fba3dedfdb495e547f19b9bd53e
konorezzova-spec <konorezzova@gmail.com>
2026-06-07 20:15:35 +0300

    ready to pass

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -9,6 +9,6 @@ export async function getImagesByQuery(query, page) {
     const API_KEY = '49108638-27579a6dba88847264bd73f39';
     const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`);
     rez.images = response.data.hits;
-    rez.totalHits = response.data.totalHits;
+    rez.totalHits = response.data.totalHits
     return rez;
 }
